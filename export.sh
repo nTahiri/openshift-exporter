11c09a3f6b232a081e96ff0d1c71e776800b6530
nTahiri <nabil.tahri@bnc.ca>
2017-03-14 09:23:31 -0400

    Update export.sh

--- export.sh
+++ export.sh
@@ -1,7 +1,8 @@
 #! /bin/bash -eu
 #OPTIONS OLD_OPENSHIFT_USERNAME OLD_OPENSHIFT_PASSWORD OLD_OPENSHIFT_URL OLD_OPENSHIFT_PROJECT
 #NEW_OPENSHIFT_USERNAME NEW_OPENSHIFT_PASSWORD NEW_OPENSHIFT_URL NEW_OPENSHIFT_PROJECT
-# /bin/sh export.sh -opts=dc,is,bc,route,svc,template -ou=old-user -opp=old-user -ourl=https://144.217.161.255:8443 -op=test -nu=new-user -npp=new-user -nurl=https://144.217.161.255:8443 -np=test
+# /bin/sh export.sh -opts=dc,is,build,svc,template -ou=old-user -opp=old-user -ourl=https://144.217.161.255:8443
+# -op=test -nu=new-user -npp=new-user -nurl=https://144.217.161.255:8443 -np=test
 
 
 main(){
@@ -21,57 +22,80 @@ executeExport(){
                 OPTIONS=all
         fi
         [ -e ${OLD_OPENSHIFT_PROJECT}.json ] && rm ${OLD_OPENSHIFT_PROJECT}.json
+        [ -e ${OLD_OPENSHIFT_PROJECT}-is.json ] && rm ${OLD_OPENSHIFT_PROJECT}-is.json
+        [ -e ${OLD_OPENSHIFT_PROJECT}-dep.json ] && rm ${OLD_OPENSHIFT_PROJECT}-dep.json
 #       cd $OPENSHIFT_OC_HOME && unset KUBECONFIG && \
                 echo "===> login to ${OLD_OPENSHIFT_URL} " && \
                 echo "====> oc login -u ${OLD_OPENSHIFT_USERNAME} -p ${OLD_OPENSHIFT_PASSWORD} ${OLD_OPENSHIFT_URL} --insecure-skip-tls-verify "
-                oc login -u ${OLD_OPENSHIFT_USERNAME} -p ${OLD_OPENSHIFT_PASSWORD} ${OLD_OPENSHIFT_URL} --insecure-skip-tls-verify && \
+                ./oc login -u ${OLD_OPENSHIFT_USERNAME} -p ${OLD_OPENSHIFT_PASSWORD} ${OLD_OPENSHIFT_URL} --insecure-skip-tls-verify && \
 
                 echo "===> connect to project ${OLD_OPENSHIFT_PROJECT}" && \
-                #/bin/oc project ${OLD_OPENSHIFT_PROJECT} && \
+                ./oc project ${OLD_OPENSHIFT_PROJECT} && \
+                 ./oc export is -o json >> ${OLD_OPENSHIFT_PROJECT}-is.json && \
+                #./oc export dc -o json >> ${OLD_OPENSHIFT_PROJECT}-dep.json && \
 
                 echo "===> export ${OPTIONS} " && \
-                oc export ${OPTIONS} -o json >> ${OLD_OPENSHIFT_PROJECT}.json && \
+                ./oc export ${OPTIONS} -o json >> ${OLD_OPENSHIFT_PROJECT}.json && \
+
+                #echo "sed -i 's/${OLD_OPENSHIFT_PROJECT}/${NEW_OPENSHIFT_PROJECT}/g' ${OLD_OPENSHIFT_PROJECT}.json" && \
+                sed -i "s/${OLD_OPENSHIFT_PROJECT}/${NEW_OPENSHIFT_PROJECT}/g" ${OLD_OPENSHIFT_PROJECT}.json && \
+                #sed -i "s/${OLD_OPENSHIFT_PROJECT}/${NEW_OPENSHIFT_PROJECT}/g" ${OLD_OPENSHIFT_PROJECT}-dep.json && \
+
+                #mv -f ${OLD_OPENSHIFT_PROJECT}-1.json ${OLD_OPENSHIFT_PROJECT}.json && \
 
                 #################################
-                jq '(. | select(.kind=="List") | .items[] | select(.kind=="Route") | .spec.host) = ""' ${OLD_OPENSHIFT_PROJECT}.json  > ${OLD_OPENSHIFT_PROJECT}-1.json
-                mv -f ${OLD_OPENSHIFT_PROJECT}-1.json ${OLD_OPENSHIFT_PROJECT}.json
+                ./jq '(. | select(.kind=="List") | .items[] | select(.kind=="Route") | .spec.host) = ""' ${OLD_OPENSHIFT_PROJECT}.json  > ${OLD_OPENSHIFT_PROJECT}-1.json && \
+                mv -f ${OLD_OPENSHIFT_PROJECT}-1.json ${OLD_OPENSHIFT_PROJECT}.json && \
 
-                jq '(. | select(.kind=="List") | .items[] | select(.kind=="Route") | .spec.status.ingress[0].host) = ""' ${OLD_OPENSHIFT_PROJECT}.json  > ${OLD_OPENSHIFT_PROJECT}-1.json
-                mv -f ${OLD_OPENSHIFT_PROJECT}-1.json ${OLD_OPENSHIFT_PROJECT}.json
-
-
+                ./jq '(. | select(.kind=="List") | .items[] | select(.kind=="Route") | .spec.status.ingress[0].host) = ""' ${OLD_OPENSHIFT_PROJECT}.json  > ${OLD_OPENSHIFT_PROJECT}-1.json && \
+                mv -f ${OLD_OPENSHIFT_PROJECT}-1.json ${OLD_OPENSHIFT_PROJECT}.json && \
 
 
-                jq --arg ns "$NEW_OPENSHIFT_PROJECT" '(. | select(.kind=="List") | .items[] | select(.kind=="BuildConfig") | .spec.strategy.sourceStrategy.from.namespace) = $ns' ${OLD_OPENSHIFT_PROJECT}.json  > ${OLD_OPENSHIFT_PROJECT}-1.json
-                mv -f ${OLD_OPENSHIFT_PROJECT}-1.json ${OLD_OPENSHIFT_PROJECT}.json
 
 
-                 jq --arg ns "$NEW_OPENSHIFT_PROJECT" '(. | select(.kind=="List") | .items[] | select(.kind=="DeploymentConfig") | .spec.triggers[] | select(.type=="ImageChange") | .imageChangeParams.from.namespace) = $ns' ${OLD_OPENSHIFT_PROJECT}.json  > ${OLD_OPENSHIFT_PROJECT}-1.json
-                mv -f ${OLD_OPENSHIFT_PROJECT}-1.json ${OLD_OPENSHIFT_PROJECT}.json
+                ./jq --arg ns "$NEW_OPENSHIFT_PROJECT" '(. | select(.kind=="List") | .items[] | select(.kind=="BuildConfig") | .spec.strategy.sourceStrategy.from.namespace) = $ns' ${OLD_OPENSHIFT_PROJECT}.json  > ${OLD_OPENSHIFT_PROJECT}-1.json && \
+                mv -f ${OLD_OPENSHIFT_PROJECT}-1.json ${OLD_OPENSHIFT_PROJECT}.json && \
 
-                 jq --arg ns "$NEW_OPENSHIFT_PROJECT" '(. | select(.kind=="List") | .items[] | select(.kind=="Template") | .objects[] | select(.kind=="BuildConfig") | .spec.strategy.sourceStrategy.from.namespace) = $ns' ${OLD_OPENSHIFT_PROJECT}.json  > ${OLD_OPENSHIFT_PROJECT}-1.json
-                mv -f ${OLD_OPENSHIFT_PROJECT}-1.json ${OLD_OPENSHIFT_PROJECT}.json
+                ./jq --arg ns "$NEW_OPENSHIFT_PROJECT" '(. | select(.kind=="List") | .items[] | select(.kind=="Template") | .objects[] | select(.kind=="BuildConfig") | .spec.strategy.sourceStrategy.from.namespace) = $ns' ${OLD_OPENSHIFT_PROJECT}.json  > ${OLD_OPENSHIFT_PROJECT}-1.json && \
+                mv -f ${OLD_OPENSHIFT_PROJECT}-1.json ${OLD_OPENSHIFT_PROJECT}.json && \
+
+
+                 ./jq --arg ns "$NEW_OPENSHIFT_PROJECT" '(. | select(.kind=="List") | .items[] | select(.kind=="DeploymentConfig") | .spec.triggers[] | select(.type=="ImageChange") | .imageChangeParams.from.namespace) = $ns' ${OLD_OPENSHIFT_PROJECT}.json  > ${OLD_OPENSHIFT_PROJECT}-1.json && \
+                mv -f ${OLD_OPENSHIFT_PROJECT}-1.json ${OLD_OPENSHIFT_PROJECT}.json && \
+
+
+                sed -i '/\"volumes\"/,/]/ d; /^$/d' ${OLD_OPENSHIFT_PROJECT}.json
+                sed -i '/\"volumeMounts\"/,/]/ d; /^$/d' ${OLD_OPENSHIFT_PROJECT}.json
+
 
                 #################################
 
                 ls && \
                 echo "===> logout ****" && \
-                oc logout && \
+                ./oc logout && \
 
                 echo "===> login to ${NEW_OPENSHIFT_URL} " && \
-                oc login -u ${NEW_OPENSHIFT_USERNAME} -p ${NEW_OPENSHIFT_PASSWORD} ${NEW_OPENSHIFT_URL} --insecure-skip-tls-verify && \
+                ./oc login -u ${NEW_OPENSHIFT_USERNAME} -p ${NEW_OPENSHIFT_PASSWORD} ${NEW_OPENSHIFT_URL} --insecure-skip-tls-verify && \
 
-                oc new-project ${NEW_OPENSHIFT_PROJECT} && \
+
+                ./oc new-project ${NEW_OPENSHIFT_PROJECT} && \
+
+                ./oc policy add-role-to-group system:image-puller system:serviceaccount:${NEW_OPENSHIFT_PROJECT} -n ${OLD_OPENSHIFT_PROJECT} && \
+
+                #./oc project ${NEW_OPENSHIFT_PROJECT} && \
 
                 #echo "===>  login to ${NEW_OPENSHIFT_URL} " && \
                 #/bin/oc delete project ${NEW_OPENSHIFT_PROJECT} && \
 
                 echo "===> connect to project ${NEW_OPENSHIFT_PROJECT} " && \
-                oc project ${NEW_OPENSHIFT_PROJECT} && \
+                ./oc project ${NEW_OPENSHIFT_PROJECT} && \
 
                 echo "===> create $(pwd)/${OLD_OPENSHIFT_PROJECT}.json " && \
-                oc create -f $(pwd)/${OLD_OPENSHIFT_PROJECT}.json
+                #./oc create -f $(pwd)/${OLD_OPENSHIFT_PROJECT}-dep.json -n ${NEW_OPENSHIFT_PROJECT}
+                ./oc create -f $(pwd)/${OLD_OPENSHIFT_PROJECT}-is.json -n ${NEW_OPENSHIFT_PROJECT}
+                ./oc create -f $(pwd)/${OLD_OPENSHIFT_PROJECT}.json -n ${NEW_OPENSHIFT_PROJECT}
 
+                ./oc logout
 }
 println(){
         echo "FILE OPTIONS  = ${OPTIONS}"
